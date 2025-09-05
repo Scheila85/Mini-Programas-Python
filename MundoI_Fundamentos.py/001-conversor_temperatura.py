d6f2ad72ff7828c2e4e3b08f37822c077c71b110
scheila <sheillapaes08@gmail.com>
2025-09-05 01:06:20 -0300

    Chore: remoção do arquivo 001-conversor_temperatura.py

--- MundoI_Fundamentos.py/001-conversor_temperatura.py
+++
@@ -1,6 +0,0 @@
-# 014) Faça um programa que leia a temperatura em °C e tranforme em °F.
-
-#Objetivo: ler temperatura em °C digitada pelo usuário e converter em °F.
-
-temperatura_graus = float(input("Qual a temperatura em °C atual (apenas números)? "))
-print(f"{temperatura_graus}°C correspondem a {(temperatura_graus * (9/5)) + 32}°F.")
