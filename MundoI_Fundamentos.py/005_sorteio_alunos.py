0b26873b32e532003476d360701310e38a617e38
scheila <sheillapaes08@gmail.com>
2025-09-06 00:19:33 -0300

    Adiciona 005_sorteio_alunos.py

---
+++ MundoI_Fundamentos.py/005_sorteio_alunos.py
@@ -0,0 +1,11 @@
+# 019 Um professor quer sortear um dos seus alunos para apagar o quadro. Faça um programa que leia 
+# o nome dos quatro alunos e escreva o nome do escolhido.
+
+from random import choice
+
+nome_1 = input("Digite o nome do 1° aluno: ").strip().title()
+nome_2 = input("Digite o nome do 2° aluno: ").strip().title()
+nome_3 = input("Digite o nome do 3° aluno: ").strip().title()
+nome_4 = input("Digite o nome do 4° aluno: ").strip().title()
+sorteado = choice([nome_1, nome_2, nome_3, nome_4])
+print(f"O aluno sorteado será: {sorteado}")
