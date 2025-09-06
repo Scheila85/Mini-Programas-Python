5d8d9b9a6bfa0cffe61e2aef83aed24428749f6c
scheila <sheillapaes08@gmail.com>
2025-09-06 01:47:34 -0300

    Adiciona 008_selecionando_letras_posicoes.py

---
+++ MundoI_Fundamentos.py/008_selecionando_letras_posicoes.py
@@ -0,0 +1,28 @@
+# Crie um programa que leia uma frase pelo teclado e mostre: 
+# Quantas vezes aparece a letra "A";
+# Em que posição ela aparece a primeira vez.
+# Em que posição ela aparece a última vez.
+
+frase = input("Digite uma frase: ").strip().lower()
+separando_string = frase.split()
+joined_lista = "".join(separando_string)
+
+print(f"A letra 'A' aparece {joined_lista.count("a")} vezes.")
+print(f"A letra 'A' aparece pela PRIMEIRA vez no caractere {joined_lista.find("a")+1}")
+# COLOCO + 1 porque eu sei que o Python começa a contar a posição a partir de 0, mas queM estará usando
+# o programa não sabe, então o ideal é que apareça de acordo com a quantidade de caracteres que a frase tem.
+
+print(f"A letra 'A' aparece pela ÚLTIMA vez no caractere {joined_lista.rfind("a")+1}.")
+
+#### Agora deixando o usuário escolher a letra que quer analisar:
+
+frase = input("Digite uma frase: ").strip().upper()
+escolhendo_letra = input("Digite a letra que você quer analisar: ").strip().upper()
+lista = frase.split()
+joined = "".join(lista)
+
+print(f"A letra '{escolhendo_letra}' aparece {joined.count(escolhendo_letra)} vezes.") 
+
+print(f"A letra '{escolhendo_letra}' aparece pela PRIMEIRA vez no caractere {joined.find(escolhendo_letra)+1}")
+
+print(f"A letra '{escolhendo_letra}' aparece pela ÚLTIMA vez no caractere {joined.rfind(escolhendo_letra)+1}.")
