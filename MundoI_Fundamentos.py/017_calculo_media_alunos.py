a395c491cfe18d53e7ee50b45dcc9e8b40f88789
scheila <sheillapaes08@gmail.com>
2025-09-07 13:12:20 -0300

    Corrige numeração de 017_calculo_media_alunos.py

--- MundoI_Fundamentos.py/017_calculo_media_alunos.py
+++ MundoI_Fundamentos.py/017_calculo_media_alunos.py
@@ -1,4 +1,4 @@
-# 040 Crie um programa que leia duas notas de um aluno e calcule a sua média, mostrando
+# Crie um programa que leia duas notas de um aluno e calcule a sua média, mostrando
 # uma mensagem no final, de acordo com a média atingida:
 # - Média abaixo de 5.0: REPROVADO
 # - Média entre 5.0 e 6.9: RECUPERAÇÃO
