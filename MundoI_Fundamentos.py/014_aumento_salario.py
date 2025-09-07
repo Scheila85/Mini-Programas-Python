b3347bae8bc79bc35025da57b2d726239f81aab2
scheila <sheillapaes08@gmail.com>
2025-09-07 03:43:15 -0300

    Adiciona 014_aumento_salario.py

---
+++ MundoI_Fundamentos.py/014_aumento_salario.py
@@ -0,0 +1,12 @@
+# 034 Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
+# Para salários superiores a R$ 1.250,00 calcule um aumento de 10%.
+# Para salários inferiores ou iguais, o aumento é de 15%.
+
+salario = float(input("Qual o valor do seu salário atual: R$ ").strip())
+
+if salario > 1250:
+    aumento = salario + (salario*(10/100))
+    print(f"Considerando um aumento de 10%, seu novo salário será de R${aumento:.1f}.")
+else:
+    aumento = salario + (salario*(15/100))
+    print(f"Considerando um aumento de 15%, seu novo salário será de R${aumento:.1f}.")
