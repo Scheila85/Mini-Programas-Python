6855571663cf7cedfe3d3bdb4e71ba12345bcf43
scheila <sheillapaes08@gmail.com>
2025-09-15 14:21:25 -0300

    Adiciona 024_somando_numeros_pares.py

---
+++ MundoII_Estruturas_de_Controle.py/024_somando_numeros_pares.py
@@ -0,0 +1,21 @@
+# Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares.
+# Se o valor digitado for ímpar desconsidere-o.
+
+print("A seguir, digite 6 números inteiros para saber a soma dos números pares: ")
+
+soma = 0
+contador = 0
+impares_desconsiderados = []
+numeros = ["primeiro número","segundo número","terceiro número","quarto número","quinto número","sexto número" ]
+
+for numero in numeros:
+    entrada = int(input(f"Digite o {numero}: "))
+    if entrada % 2 == 0:
+        soma += entrada
+        contador += 1  #implementei Guanabara
+    else:
+        impares_desconsiderados.append(entrada)  #Tinha somado, mas estava errado. 
+#Este é o método quando quero adicionar valores em uma lista vazia.
+
+print(f"Os números ímpares {impares_desconsiderados} digitados foram desconsiderados.")
+print(f"Você digitou {contador} números par (es) e a soma é: {soma}.")
